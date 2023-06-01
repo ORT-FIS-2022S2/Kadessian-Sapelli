69d9e8b7273cb60a4c96f425d3c2ab3f5818c1c1
jojo <jojosapellif@gmail.com>
2023-06-01 14:56:07 -0300

    add: imports index.js

---
+++ interfaz/js/index.js
@@ -0,0 +1,7 @@
+import { Dia } from './dominio/Dia.js';
+import { Menu } from './dominio/Menu.js';
+import { Padre } from './dominio/Padre.js';
+import { Comentario } from './dominio/Comentario.js';
+import { Sistema } from './dominio/Sistema.js';
+import { sistema } from './dominio/datosPrecargados.js';
+const parrafoDatosMenu = document.getElementById("parrafoDatosMenu");
